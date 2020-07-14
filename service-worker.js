624b904b0c58a78270be89e42ac35b69bb5c5b86
Rohit Patil <rahulhimesh09@gmail.com>
2020-07-14 18:26:52 +0530

    change

--- service-worker.js
+++ service-worker.js
@@ -26,7 +26,35 @@ self.addEventListener("install", function(event) {
 
   event.waitUntil(caches.open("web-app").then(function(cache) {
 
-    return cache.addAll(['404.html', 'archive.html', 'styles.html', 'about.html', 'index.html','Branch-to-Test.html','index.html','getting-started-with-python.html','setting-up-your-blog-on-github-pages.html','startup-automation.html','evolution-in-psuedo-IDE.html', 'lets-begin.html','apple-touch-icon-precomposed.png', 'browser_image.png', 'manifest.json','/assets/css/styles.css.map', '/assets/css/styles.css','Using-Git-The-Basics.html','Simplify-Workings-Using-SSH.html']).then(function() {
+    return cache.addAll(["./404.html",
+    "./about.html",
+    "./assets/apple-touch-icon-precomposed.png",
+    "./assets/browser_image.png",
+    "./assets/css/styles.css",
+    "./assets/css/styles.css.map",
+    "./assets/images",
+    "./assets/images/Git-commit-branch.svg",
+    "./assets/images/Github-Make-Repo.jpeg",
+    "./assets/images/Github-online.jpeg",
+    "./assets/images/evolution-sessions.jpg",
+    "./assets/images/evolution-tabs-micro.jpg",
+    "./assets/images/lets-begin1.jpg",
+    "./assets/images/lets-begin2.jpg",
+    "./assets/images/lets-begin3i.png",
+    "./assets/images/lets-begin3ii.png",
+    "./assets/images/lets-begin3iii.png",
+    "./assets/images/lets-begin4.jpg",
+    "./assets/images/new-ssh-key-add.png",
+    "./assets/images/new-ssh-key.png",
+    "./assets/manifest.json",
+    "./categories/index.html",
+    "./git/Using-Git-The-Basics.html",
+    "./git/github/Simplify-Workings-Using-SSH.html",
+    "./index.html",
+    "./linux/shell scripting/startup-automation.html",
+    "./tags/index.html",
+    "./termux/evolution-in-psuedo-IDE.html",
+    "./termux/lets-begin.html"]).then(function() {
 
       self.skipWaiting();
 
